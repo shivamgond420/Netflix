81fa3a95787561c3f5d22f1416bd6aede592f715
shivamgond420 <136161872+shivamgond420@users.noreply.github.com>
2023-08-06 07:55:55 +0530

    this is updated

--- src/app/movie/page.jsx
+++ src/app/movie/page.jsx
@@ -1,40 +1,50 @@
-
 "use client";
-import React from "react";
-import Link from "next/link";
+import React, { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
-import { resolve } from "styled-jsx/css";
 
-const Contact = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  const url =
-    "https://netflix54.p.rapidapi.com/search/?query=stranger&offset=0&limit_titles=50&limit_suggestions=20&lang=en";
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "84edb2b4a3mshc3c9d915214437fp186444jsnea11975918ef",
-      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
-    },
+const Contact = () => {
+  const [mainData, setMainData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const url =
+      "https://netflix54.p.rapidapi.com/search/?query=stranger&offset=0&limit_titles=50&limit_suggestions=20&lang=en";
+    const options = {
+      method: "GET",
+      headers: {
+        'X-RapidAPI-Key': '12944cb6f7mshe86951629eb3061p15cb16jsn118dfa245597',
+        'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const res = await fetch(url, options);
+      const data = await res.json();
+      const main_data = data.titles;
+      setMainData(main_data);
+      console.log(main_data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
-  const res = await fetch(url, options);
-  const data = await res.json();
-  const main_data = data.titles;
-
   return (
-   
-    <div class="grid-container p-4 gap-4 md:grid-cols-2 lg:grid-cols-3">
-  <div class="text-center font-bold text-gray-900 text-2xl col-span-full">
-    Series & Movies
-  </div>
-  <div class="movie-grid  p-4 gap-4 grid-cols-2  grid-rows-3">
-  {main_data.map((cruEle) => {
-          return <MovieCard key={cruEle.id} {...cruEle} />;
-    })}
-  </div>
-</div>
-
+    <div className="grid-container p-4 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="text-center font-bold text-gray-900 text-2xl col-span-full">
+        Series & Movies
+      </div>
+      <div className="movie-grid p-4 gap-4 grid-cols-2 grid-rows-3">
+        {mainData ? (
+          mainData.map((cruEle) => <MovieCard key={cruEle.id} {...cruEle} />)
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
   );
 };
 
